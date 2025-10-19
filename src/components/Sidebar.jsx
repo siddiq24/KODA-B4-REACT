8f85391a6e098fcd7352f654d5f2be08d8b50362
siddiq24 <siddiqrm24@gmail.com>
2025-10-19 23:38:32 +0700

    fix(sidebar-nav) : add navigate to login and register

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router'
 function Sidebar({ isOpen, set }) {
     return (
         <div onClick={(e) => e.stopPropagation()}
-            className={`fixed h-screen z-20 top-0 w-[80%] max-w-sm bg-white p-3 px-5 transition-all duration-1000 ease-in-out flex flex-col ${isOpen ? 'left-0' : '-left-full'
+            className={`fixed h-screen z-20 top-0 w-[80%] max-w-sm pb-8 bg-white p-3 px-5 transition-all duration-1000 ease-in-out flex flex-col ${isOpen ? 'left-0' : '-left-full'
                 }`}>
             <Logo color={'#8E6447'} w={150} h={50} />
             <nav className="mt-8 mb-auto">
@@ -38,8 +38,8 @@ function Sidebar({ isOpen, set }) {
 
             </nav>
             <nav className='flex-1 flex flex-col justify-end gap-4 pb-8'>
-                <Link to={'#'} className='w-full text-center text-xl border rounded-lg p-4'>Sign In</Link>
-                <Link to={'#'} className='w-full text-center text-xl border border-[#ff8906] rounded-lg p-4 bg-[#ff8906]'>Sign Up</Link>
+                <Link to={'/auth/login'} className='w-full text-center text-xl border rounded-lg p-4'>Sign In</Link>
+                <Link to={'/auth/register'} className='w-full text-center text-xl border border-[#ff8906] rounded-lg p-4 bg-[#ff8906]'>Sign Up</Link>
             </nav>
         </div>
     )
