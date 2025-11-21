522414d18b3f3d4a4b8fa544cf440b526e70115a
siddiq24 <siddiqrm24@gmail.com>
2025-11-22 04:13:00 +0700

    fix: delete underscore to match request

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -24,7 +24,7 @@ export default function Profile() {
         formState: { errors, isDirty }
     } = useForm({
         defaultValues: {
-            name: userFromRedux?.full_name,
+            name: userFromRedux?.fullname,
             email: userFromRedux?.email,
             phone: userFromRedux?.phone,
             password: "*************",
